d4dd8df89e86841c108b7dc87e23a6d1f93d3c9f
DUC_PC <minhducvu.212@gmail.com>
2018-07-24 14:40:51 +0700

    init project resource

---
+++ app/src/main/java/com/example/ducvu212/gallary/IList.java
@@ -0,0 +1,6 @@
+package com.example.ducvu212.gallary;
+
+public interface IList {
+    Item getItem(int position) ;
+    int getItemCount() ;
+}
---
+++ app/src/main/java/com/example/ducvu212/gallary/Item.java
@@ -0,0 +1,18 @@
+package com.example.ducvu212.gallary;
+
+public class Item {
+
+    private int mId;
+
+    public Item(int id) {
+        mId = id;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+}
