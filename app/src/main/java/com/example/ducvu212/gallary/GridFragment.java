c17d5ea05f91b8842751a424329b17f4ca0191c6
DUC_PC <minhducvu.212@gmail.com>
2018-07-24 22:42:59 +0700

    Get data and show

---
+++ app/src/main/java/com/example/ducvu212/gallary/GridFragment.java
@@ -0,0 +1,104 @@
+package com.example.ducvu212.gallary;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.ArrayList;
+
+import static com.example.ducvu212.gallary.MainActivity.sPaths;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GridFragment extends Fragment implements IList {
+
+    private ImageAdapter mAdapterImage;
+    private ArrayList<ItemImage> mItemArrayList;
+    private RecyclerView mRecyclerView;
+    private int mFirstItem = 0;
+
+    public GridFragment() {
+        // Required empty public constructor
+    }
+
+    public static GridFragment newInstance() {
+        return new GridFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getActivity().getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        return inflater.inflate(R.layout.fragment_grid, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        findViewByIds();
+        addData();
+        initComponents();
+    }
+
+    private void addData() {
+        for (int i = mFirstItem; i < mFirstItem + 10; i++) {
+            mItemArrayList.add(new ItemImage(sPaths.get(i)));
+        }
+        mFirstItem = mItemArrayList.size() - 1;
+    }
+
+    private void initComponents() {
+        final RecyclerView.LayoutManager manager = new GridLayoutManager(getContext(), 2);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mAdapterImage = new ImageAdapter(mItemArrayList, this, getActivity(), mRecyclerView, manager);
+        mRecyclerView.setAdapter(mAdapterImage);
+        mRecyclerView.smoothScrollToPosition(0);
+        mAdapterImage.setLoadMore(new ImageAdapter.ILoadMore() {
+            @Override
+            public void onLoadMore() {
+                mItemArrayList.add(null);
+                mAdapterImage.notifyItemInserted(mItemArrayList.size() - 1);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mItemArrayList.remove(mItemArrayList.size() - 1);
+                        mAdapterImage.notifyItemRemoved(mItemArrayList.size());
+                        mAdapterImage.notifyDataSetChanged();
+                        addData();
+                        mAdapterImage.setLoaded();
+                    }
+                }, 2000);
+            }
+        });
+    }
+
+    private void findViewByIds() {
+        mRecyclerView = getActivity().findViewById(R.id.recycleview_grid);
+        mItemArrayList = new ArrayList<>();
+    }
+
+    @Override
+    public ItemImage getItem(int position) {
+        return mItemArrayList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItemArrayList.size();
+    }
+}
--- app/src/main/java/com/example/ducvu212/gallary/MainActivity.java
+++ app/src/main/java/com/example/ducvu212/gallary/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.ducvu212.gallary;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int READ_STORAGE_PERMISSION = 111;
     private boolean mIsSdCard;
+    public static ArrayList<String> sPaths;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
@@ -22,20 +31,46 @@ public class MainActivity extends AppCompatActivity {
         checkPermissions();
         findViewByIds();
         initComponents();
+        if (mIsSdCard) {
+            loadImages(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            loadImages(MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        } else {
+            loadImages(MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        }
+    }
+
+    private ArrayList<String> loadImages(Uri uri) {
+        final String[] columns = {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID};
+        final String orderBy = MediaStore.Images.Media.DATE_ADDED;
+        Cursor cursor = getContentResolver().query(uri, columns, null, null, orderBy);
+        int count = cursor.getCount();
+        String[] arrPath = new String[count];
+        for (int i = 0; i < count; i++) {
+            cursor.moveToPosition(i);
+            int dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+            arrPath[i] = cursor.getString(dataColumnIndex);
+        }
+        cursor.close();
+        sPaths.addAll(Arrays.asList(arrPath));
+        return sPaths;
+    }
+
+    private void initComponents() {
+        mIsSdCard = android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
+        sPaths = new ArrayList<>();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.frame_main, GridFragment.newInstance());
+        transaction.commit();
     }
 
     private void findViewByIds() {
 
     }
 
-    private void initComponents() {
-        mIsSdCard = android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     private void checkPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_STORAGE_PERMISSION);
         }
     }
